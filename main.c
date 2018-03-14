e9e214c11792063a8f20dcf3506a721ec90d10c6
cocafe <cocafehj@gmail.com>
2018-03-14 17:05:31 +0800

    main: add some comments
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- main.c
+++ main.c
@@ -15,39 +15,52 @@ int main()
 {
         GLFWwindow *window;
 
+        // GLFW init
         if (!glfwInit()) {
                 return EXIT_FAILURE;
         }
 
+        // GLFW window option: MSAA
         glfwWindowHint(GLFW_SAMPLES, 4);
 
+        // GLFW OpenGL context options
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+        // GLFW create window
         window = glfwCreateWindow(1280, 720, "MyCraft Demo", NULL, NULL);
         if (!window) {
                 glfwTerminate();
                 return EXIT_FAILURE;
         }
 
+        // GLFW make OpenGL context
         glfwMakeContextCurrent(window);
 
+        // GLEW init
         glewExperimental = true;
         if (glewInit() != GLEW_OK) {
                 glfwTerminate();
                 return EXIT_FAILURE;
         }
 
+        // GLFW window input options
         glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-        glfwPollEvents();
         glfwSetCursorPos(window, 1280 / 2, 720 / 2);
 
+        // GLFW flush unhandled events
+        glfwPollEvents();
+
+        // GLFW window vsync settings:
+        //      -1: auto
+        //      1: on
+        //      0: off
         glfwSwapInterval(-1);
 
+        // OpenGL background color: RGBA
         glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
 
         do {
