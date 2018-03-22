5fb6e0f78d35ee538b95929276cdb942ab9684d5
cocafe <cocafehj@gmail.com>
2018-03-22 10:13:58 +0800

    utils: add file suffix macros
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,12 @@
 #ifndef MYCRAFT_DEMO_UTIL_H
 #define MYCRAFT_DEMO_UTIL_H
 
+#define SUFFIX_PNG                      ".png"
+#define SUFFIX_BMP                      ".bmp"
+#define SUFFIX_TGA                      ".tga"
+#define SUFFIX_DDS                      ".dds"
+#define SUFFIX_OBJ                      ".obj"
+
 char *file_read(const char *filepath);
 char *buf_alloc(size_t len);
 int buf_free(char **buf);
