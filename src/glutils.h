1f750c243635050aeef13878716588d566a0110f
cocafe <cocafehj@gmail.com>
2018-03-22 09:53:13 +0800

    glutils: gl empty buffer macros
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/glutils.h
+++ src/glutils.h
@@ -1,6 +1,12 @@
 #ifndef MYCRAFT_DEMO_GLUTILS_H
 #define MYCRAFT_DEMO_GLUTILS_H
 
+#define GL_SHADER_NONE                  (0)
+#define GL_BUFFER_NONE                  (0)
+#define GL_SAMPLER_NONE                 (0)
+#define GL_TEXTURE_NONE                 (0)
+#define GL_PROGRAM_NONE                 (0)
+
 GLuint shader_compile(GLenum type, const char *source);
 GLuint shader_load(GLenum type, const char *filepath);
 GLuint program_link(GLuint shader_vertex, GLuint shader_frag);
