f94c65cd49e2f08954b34af66fcdcc0688376dbf
cocafe <cocafehj@gmail.com>
2018-03-26 16:08:32 +0800

    debug: add debug level control
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/debug.h
+++ src/debug.h
@@ -1,11 +1,17 @@
 #ifndef MYCRAFT_DEMO_DEBUG_H
 #define MYCRAFT_DEMO_DEBUG_H
 
-#define DEBUG_PRINTF
+#define PRINT_INFO_BIT                  (1 << 0)
+#define PRINT_ERROR_BIT                 (1 << 1)
+#define PRINT_DEBUG_BIT                 (1 << 2)
+
+extern int g_debug_level;
 
 #define pr_info(...)            do {                                            \
-                                        fprintf(stdout, __VA_ARGS__);           \
-                                        fflush(stdout);                         \
+                                        if (g_debug_level & PRINT_INFO_BIT) {   \
+                                                fprintf(stdout, __VA_ARGS__);   \
+                                                fflush(stdout);                 \
+                                        }                                       \
                                 } while (0)
 
 #define pr_info_func(...)       do {                                            \
@@ -13,24 +19,23 @@
                                         pr_info(__VA_ARGS__);                   \
                                 } while (0)
 
-#ifdef DEBUG_PRINTF
 #define pr_debug(...)           do {                                            \
-                                        fprintf(stdout, __VA_ARGS__);           \
-                                        fflush(stdout);                         \
+                                        if (g_debug_level & PRINT_DEBUG_BIT) {  \
+                                                fprintf(stdout, __VA_ARGS__);   \
+                                                fflush(stdout);                 \
+                                        }                                       \
                                 } while (0)
 
 #define pr_debug_func(...)      do {                                            \
                                         fprintf(stdout, "%s(): ", __func__);    \
                                         pr_debug(__VA_ARGS__);                  \
                                 } while (0)
-#else
-#define pr_debug(...)           do { } while (0)
-#define pr_dbueg_func(...)      do { } while (0)
-#endif
 
 #define pr_err(...)             do {                                            \
-                                        fprintf(stderr, __VA_ARGS__);           \
-                                        fflush(stderr);                         \
+                                        if (g_debug_level & PRINT_ERROR_BIT) {  \
+                                                fprintf(stderr, __VA_ARGS__);   \
+                                                fflush(stderr);                 \
+                                        }                                       \
                                 } while (0)
 
 #define pr_err_func(...)        do {                                            \
