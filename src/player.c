ddb1e52590957bc42743b13c3e4a64054ced1740
cocafe <cocafehj@gmail.com>
2018-03-28 16:29:16 +0800

    player: add camera perspective trasformation
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/player.c
+++ src/player.c
@@ -1,6 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <memory.h>
 #include <errno.h>
+#include <math.h>
 
+#include <GLFW/glfw3.h>
+
+#include "debug.h"
+#include "block.h"
+#include "model.h"
+#include "utils.h"
+#include "glutils.h"
 #include "player.h"
+
+int camera_init(camera *cam, camera *hint)
+{
+        if (!cam || !hint)
+                return -EINVAL;
+
+        memcpy(cam, hint, sizeof(camera));
+
+        // TODO: Adjust camera positon upon player position
+
+        return 0;
+}
+
+void camera_deltatime_compute(camera *cam)
+{
+        cam->time_curr = glfwGetTime();
+        cam->time_delta = (float)(cam->time_curr - cam->time_last);
+        cam->time_last = cam->time_curr;
+}
+
+static inline int cursor_is_changed(double a, double b)
+{
+        return fabs(a - b) < 0.000001 ? 0 : 1;
+}
+
+void camera_vectors_compute(GLFWwindow *window, camera *cam, double speed)
+{
+        double x, y;
+        double set_x, set_y;
+
+        // TODO: GLFW Callback
+        glfwGetWindowSize(window, &cam->window_width, &cam->window_height);
+
+        set_x = (double)cam->window_width / 2;
+        set_y = (double)cam->window_height / 2;
+
+        // Delta of cursor movement related to screen center
+        glfwGetCursorPos(window, &x, &y);
+        glfwSetCursorPos(window, set_x, set_y);
+
+//        if (!cursor_is_changed(set_x, x) && !cursor_is_changed(set_y, y))
+//                return;
+
+        cam->angel_horizontal += speed * (set_x - x);
+        cam->angel_vertical += speed * (set_y - y);
+
+        // Front vector:
+        // Spherical coordinates to Cartesian coordinates conversion
+        cam->vector_front[X] = (float)(cos(cam->angel_vertical) * sin(cam->angel_horizontal));
+        cam->vector_front[Y] = (float)(sin(cam->angel_vertical));
+        cam->vector_front[Z] = (float)(cos(cam->angel_vertical) * cos(cam->angel_horizontal));
+
+        // Right vector:
+        // Relative to camera space
+        cam->vector_right[X] = (float)(sin(cam->angel_horizontal - M_PI / 2.0));
+        cam->vector_right[Y] = 0.0f;
+        cam->vector_right[Z] = (float)(cos(cam->angel_horizontal - M_PI / 2.0));
+
+        // Up vector:
+        // Perpendicular to front and right vector, cross production to compute
+        glm_cross(cam->vector_right, cam->vector_front, cam->vector_up);
+}
+
+// TODO: Handle custom keys...? --> GLFW_KEY_CALLBACK (need thread safe?)
+void camera_position_compute(GLFWwindow *window, camera *cam, float speed)
+{
+        vec3 t = {0, 0, 0};
+
+        // TODO: Movement call to a function to handle movement not introduce by inputs
+
+        // Forward
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+                // position(x, y, z) += front(x, y, z) * delta_time * speed;
+                glm_vec_scale(cam->vector_front, cam->time_delta, t);
+                glm_vec_scale(t, speed, t);
+                glm_vec_add(cam->position, t, cam->position);
+        }
+
+        // Backward
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+                // position(x, y, z) -= front(x, y, z) * delta_time * speed;
+                glm_vec_scale(cam->vector_front, cam->time_delta, t);
+                glm_vec_scale(t, speed, t);
+                glm_vec_sub(cam->position, t, cam->position);
+        }
+
+        // Left
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+                // position(x, y, z) -= right(x, y, z) * delta_time * speed;
+                glm_vec_scale(cam->vector_right, cam->time_delta, t);
+                glm_vec_scale(t, speed, t);
+                glm_vec_sub(cam->position, t, cam->position);
+        }
+
+        // Right
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+                // position(x, y, z) += right(x, y, z) * delta_time * speed;
+                glm_vec_scale(cam->vector_right, cam->time_delta, t);
+                glm_vec_scale(t, speed, t);
+                glm_vec_add(cam->position, t, cam->position);
+        }
+
+        // TODO: character position update
+}
+
+// TODO: dynmaic fov during sprint, flying
+void camera_perspective_compute(camera *cam, int32_t dynamic_fov)
+{
+        vec3 look_at = { 0, 0, 0 };
+
+        glm_perspective(dynamic_fov ? cam->fov + cam->fov_delta : cam->fov,
+                        (float)cam->window_width / (float)cam->window_height,
+                        cam->clamp_near, cam->clamp_far, cam->mat_persp);
+
+        glm_vec_add(cam->position, cam->vector_front, look_at);
+        glm_lookat(cam->position, look_at, cam->vector_up, cam->mat_camera);
+
+        glm_mat4_mulN((mat4 *[]){&cam->mat_persp, &cam->mat_camera},
+                      2, cam->mat_transform);
+}
+
+void camera_inputs_handle(GLFWwindow *window, camera *cam,
+                          double speed_move, double speed_view)
+{
+        // TODO: Check for game pause -> release cursor
+
+        camera_deltatime_compute(cam);
+        camera_position_compute(window, cam, (float)speed_move);
+        camera_vectors_compute(window, cam, speed_view);
+        camera_perspective_compute(cam, 0);
+}
--- src/player.h
+++ src/player.h
@@ -1,4 +1,32 @@
 #ifndef MYCRAFT_DEMO_PLAYER_H
 #define MYCRAFT_DEMO_PLAYER_H
 
+#include <cglm/cglm.h>
+
+typedef struct camera {
+        vec3            position;
+        float           fov;
+        float           fov_delta;
+        int32_t         window_width;
+        int32_t         window_height;
+        double          angel_horizontal;
+        double          angel_vertical;
+        mat4            mat_persp;
+        mat4            mat_camera;
+        mat4            mat_transform;
+        double          time_last;
+        double          time_curr;
+        float           time_delta;
+        float           clamp_near;
+        float           clamp_far;
+        vec3            vector_front;
+        vec3            vector_up;
+        vec3            vector_right;
+        // TODO: mutex lock / RCU lock
+        // TODO: TPS camera --> ray hittest
+} camera;
+
+void camera_inputs_handle(GLFWwindow *window, camera *cam,
+                          double speed_move, double speed_view);
+
 #endif //MYCRAFT_DEMO_PLAYER_H
