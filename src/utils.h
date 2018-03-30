6b19525147f9b6df0be744bfa0c84722ef7b1552
cocafe <cocafehj@gmail.com>
2018-03-30 12:07:29 +0800

    utils: add types for integer vector
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -14,6 +14,10 @@
 
 typedef float vec2[2];
 
+typedef int32_t ivec2[2];
+//typedef int32_t ivec3[3];     // cglm has defined one
+typedef int32_t ivec4[4];
+
 typedef float *pvec2;
 typedef float *pvec3;
 typedef float *pvec4;
